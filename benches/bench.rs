4d6995ea831829af5ecdef41776b1b0cfdac3bfe
Dean Karn <dean.karn@gmail.com>
2022-04-01 11:46:56 -0700

    add lex benchmarks

--- benches/bench.rs
+++ benches/bench.rs
@@ -2,9 +2,53 @@
 extern crate criterion;
 
 use criterion::{Criterion, Throughput};
+use ksql::lexer::Tokenizer;
 use ksql::parser::Parser;
 
-fn criterion_benchmark(c: &mut Criterion) {
+fn benchmark_lexer(c: &mut Criterion) {
+    let mut group = c.benchmark_group("lex_individual");
+    for (name, src) in [
+        ("identifier", ".field1".as_bytes()),
+        ("string", r#""My Name""#.as_bytes()),
+        ("number", "123.34".as_bytes()),
+        ("bool", "true".as_bytes()),
+        ("single_ident", "=".as_bytes()),
+        ("double_ident", ">=".as_bytes()),
+        ("string_ident", "CONTAINS".as_bytes()),
+    ]
+    .iter()
+    {
+        group.throughput(Throughput::Bytes(src.len() as u64));
+        group.bench_function(*name, |b| {
+            b.iter(|| {
+                let _res = Tokenizer::tokenize(src);
+            })
+        });
+    }
+    group.finish();
+
+    let mut group = c.benchmark_group("lex_expression");
+    for (name, src) in [
+        (
+            "string_ident_add_multi",
+            r#".first_name + " " + .last_name"#.as_bytes(),
+        ),
+        ("math", r#"1 + 1 / 2"#.as_bytes()),
+        ("math_paren", r#"(1 + 1) / 2"#.as_bytes()),
+    ]
+    .iter()
+    {
+        group.throughput(Throughput::Bytes(src.len() as u64));
+        group.bench_function(*name, |b| {
+            b.iter(|| {
+                let _res = Tokenizer::tokenize(src);
+            })
+        });
+    }
+    group.finish();
+}
+
+fn benchmark_expressions(c: &mut Criterion) {
     let mut group = c.benchmark_group("add");
     for (name, src, expression) in [
         ("num_num", "".as_bytes(), "1 + 1"),
@@ -64,5 +108,5 @@ fn criterion_benchmark(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group!(benches, benchmark_expressions, benchmark_lexer);
 criterion_main!(benches);
