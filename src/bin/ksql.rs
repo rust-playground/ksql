9a4744d7af1b2c6a6fd0a19a64c946051839f26b
Dean Karn <dean.karn@gmail.com>
2023-02-05 10:45:39 -0800

    add ability to return original data

--- src/bin/ksql.rs
+++ src/bin/ksql.rs
@@ -1,11 +1,17 @@
 use clap::Parser as ClapParser;
-use ksql::parser::Parser;
+use ksql::parser::{Parser, Value};
 use std::env;
 use std::io::{stdin, stdout, BufRead, Write};
 
 #[derive(Debug, ClapParser)]
 #[clap(version = env!("CARGO_PKG_VERSION"), author = env!("CARGO_PKG_AUTHORS"), about = env!("CARGO_PKG_DESCRIPTION"))]
 pub struct Opts {
+    /// Indicates if the original data will be output after applying the expression.
+    ///
+    /// The results of the expression MUST be a boolean otherwise the output will be ignored.
+    #[clap(short, long, default_value = "false")]
+    pub output_original: bool,
+
     /// ksql expression to apply to input.
     #[clap()]
     pub expression: String,
@@ -18,34 +24,50 @@ pub struct Opts {
 fn main() -> anyhow::Result<()> {
     let opts: Opts = Opts::parse();
 
-    let is_pipe = !atty::is(atty::Stream::Stdin);
-
     let ex = Parser::parse(&opts.expression)?;
 
     let stdout = stdout();
     let mut stdout = stdout.lock();
 
-    if is_pipe {
+    if let Some(data) = opts.data {
+        let bytes = data.as_bytes();
+        let v = ex.calculate(bytes)?;
+        if opts.output_original {
+            match v {
+                Value::Bool(output_original) if output_original => {
+                    stdout.write_all(bytes)?;
+                    let _ = stdout.write(&[b'\n'])?;
+                }
+                _ => {}
+            }
+        } else {
+            serde_json::to_writer(&mut stdout, &v)?;
+            let _ = stdout.write(&[b'\n'])?;
+        }
+    } else {
         let stdin = stdin();
         let mut stdin = stdin.lock();
         let mut data = Vec::new();
 
-        while stdin.read_until(b'\n', &mut data)? > 0 {
-            let v = ex.calculate(&data)?;
-            serde_json::to_writer(&mut stdout, &v)?;
-            let _ = stdout.write(&[b'\n'])?;
-            data.clear();
-        }
-        Ok(())
-    } else {
-        match opts.data {
-            None => Err(anyhow::anyhow!("No data provided")),
-            Some(data) => {
-                let v = ex.calculate(data.as_bytes())?;
+        if opts.output_original {
+            while stdin.read_until(b'\n', &mut data)? > 0 {
+                let v = ex.calculate(&data)?;
+                match v {
+                    Value::Bool(output_original) if output_original => {
+                        stdout.write_all(&data)?;
+                    }
+                    _ => {}
+                }
+                data.clear();
+            }
+        } else {
+            while stdin.read_until(b'\n', &mut data)? > 0 {
+                let v = ex.calculate(&data)?;
                 serde_json::to_writer(&mut stdout, &v)?;
                 let _ = stdout.write(&[b'\n'])?;
-                Ok(())
+                data.clear();
             }
         }
     }
+    Ok(())
 }
